7dde4722dd36e6f4b24d6a3cfcf5daa4d99d1d28
Akhil Mohan <akhil.mohan@mayadata.io>
2020-10-20 17:07:12 +0530

    use v1 and v2 together

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/akhilerm/client-test
 
 go 1.14
 
-require github.com/akhilerm/api-test/v2 v2.0.0
+require (
+	github.com/akhilerm/api-test v1.0.0
+	github.com/akhilerm/api-test/v2 v2.0.0
+)
--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
-	"github.com/akhilerm/api-test/v2/pkg/calc"
+	"github.com/akhilerm/api-test/pkg/calc"
+	calcv2 "github.com/akhilerm/api-test/v2/pkg/calc"
 )
 
 func main() {
-	v := calc.NewVariables(12, 13, 14)
-	fmt.Printf("Sum = %d \n", v.Add())
+
+	v1 := calc.NewVariables(12, 13)
+	fmt.Printf("Sum = %d \n", v1.Add())
+
+	v2 := calcv2.NewVariables(12, 13, 14)
+	fmt.Printf("Multiply = %d \n", v2.Multiply())
 }
